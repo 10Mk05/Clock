1dbad3661d14555fbdd07ef91d8865eea8019e2d
10Mk05 <93537444+10Mk05@users.noreply.github.com>
2022-11-15 19:29:54 +0100

    Add files via upload
    Messo prima il giorno , mese , anno , ora
    aggiornato parte di script per il funzionamento del mese, dell'anno, dell'ora

--- clock/main.lua
+++ clock/main.lua
@@ -1,13 +1,22 @@
 function love.load()
-    data = os.date(os.date ("%c"))
+    --data = os.date(os.date ("%c"))
     font = love.graphics.newFont("Bungee-Regular.ttf", 40)
     larghezza = love.graphics.getWidth()/2 - 200
-    nowTable = os.date(os.date ("%c"))
-    nowYear = nowTable.year
-    nowHour = nowTable.hour
-    nowHour = nowTable.hour
-    nowSeconds = nowTable.sec
-    nowMinutes = nowTable.min
+    --nowTable = os.date(os.date ("%c"))
+    --nowYear = nowTable.year
+    --nowHour = nowTable.hour
+    --nowHour = nowTable.hour
+    --nowSeconds = nowTable.sec
+    --nowMinutes = nowTable.min
+    mesi = os.date(os.date ("%m"))
+    anni = os.date(os.date ("%Y"))
+    giorni = os.date(os.date ("%d"))
+    ora = os.date(os.date ("%X"))
+    secondi = os.date(os.date ("%S"))
+    minuti = os.date(os.date ("%M"))
+    ore = os.date(os.date ("%H"))
+
+    slash = "/"
 
     sprites = {}
     sprites.clock = love.graphics.newImage('sprites/oroloio.png')
@@ -31,7 +40,14 @@ function love.load()
     sounds.TicToc:play()
     sounds.TicToc:setVolume(0.5)
 
+    
+
+
+
     love.window.setTitle("Clock")
+
+    
+
 
 end
 
@@ -43,6 +59,14 @@ function love.update(dt)
     nowSeconds = nowTable.sec
     nowMinutes = nowTable.min
     data = os.date()
+
+    mesi = os.date(os.date ("%m"))
+    anni = os.date(os.date ("%Y"))
+    giorni = os.date(os.date ("%d"))
+    ora = os.date(os.date ("%X"))
+    secondi = os.date(os.date ("%S"))
+    minuti = os.date(os.date ("%M"))
+    ore = os.date(os.date ("%H"))
     
     if nowHour >= 18 or nowHour <= 7 then
         if sounds.mare:isPlaying() == true then
@@ -52,7 +76,7 @@ function love.update(dt)
         sounds.notte:play()
 
     end
-    if nowHour >= 7 and nowHour <= 18 then
+    if nowMinutes >= 7 and nowMinutes <= 18 then
         if sounds.notte:isPlaying() == true then
             sounds.notte:stop()
         end
@@ -77,8 +101,14 @@ function love.draw()
     if nowHour >=21 and nowHour <=24 or nowHour >= 0 and nowHour <6 then
         love.graphics.draw(sprites.notte,0,0)
     end
-    love.graphics.draw(sprites.clock, larghezza-115 ,love.graphics:getHeight()-600 )
+    love.graphics.draw(sprites.clock, larghezza-100, love.graphics:getHeight()-580)
+
 
     love.graphics.setFont(font)
-    love.graphics.print(data,larghezza,love.graphics:getHeight()-515)
+    love.graphics.print(giorni,larghezza-50,love.graphics:getHeight()-500)
+    love.graphics.print(slash,larghezza,love.graphics:getHeight()-500)
+    love.graphics.print(mesi,larghezza+15,love.graphics:getHeight()-500)
+    love.graphics.print(slash,larghezza+65,love.graphics:getHeight()-500)
+    love.graphics.print(anni,larghezza+80,love.graphics:getHeight()-500)
+    love.graphics.print(ora,larghezza+250,love.graphics:getHeight()-500)
 end
